a49c71cf12d26bc01db7bfe5544b874d7dcd1abe
cora <coradelamouche@gmx.ch>
2026-06-12 02:45:05 +0200

    tests: remove witherbot and player_radar tests (mods removed)

--- clientmods/al_test/test_player_radar.lua
+++
@@ -1,8 +0,0 @@
--- Tests for player_radar mod
-
-function test_player_radar(T)
-	T.run("player_radar setting exists", function()
-		local val = core.settings:get("player_radar")
-		T.assert(val ~= nil, "player_radar setting should exist")
-	end)
-end
